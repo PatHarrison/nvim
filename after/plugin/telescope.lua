f1b5e105be68b1206c1e9778ee86f28ef9fa8110
PatHarrison <patrickgharrison@outlook.com>
2024-01-31 13:52:34 -0700

    key maps for telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,2 +1,17 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+
+require('telescope').setup{
+	defaults = {
+		mappings = {
+			i = {
+				["<C-h>"] = "which_key",
+				["<C-j>"] = require('telescope.actions').move_selection_next,
+				["<C-k>"] = require('telescope.actions').move_selection_previous,
+				}
+			}
+		},
+	pickers = {},
+	extensions = {}
+}
